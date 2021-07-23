9e74598468eea1f9e23c3984252334d8b188d536
BigFelk <fedferrara89@gmail.com>
2021-07-23 15:14:22 +0200

    finito tutto

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// funzione palindroma
 function palindrom(parola){
     var lunghezza = parola.length;
     for (var i = 0; i < lunghezza; i++){
@@ -9,30 +10,55 @@ function palindrom(parola){
     return 'palindromo';
 }
 
+// funzione random
+
+function rdn(){
+    return Math.floor(Math.random()*5) + 1
+}
+
+
+// funzione somma e controllo pari dispari
+function pa_di (num1, num2){
+
+    if (((num1 + num2) % 2) == 0){
+        return 'pari';
+    }
+    else{
+    return 'dispari';}
+}
 
 
 
 
 
 
+// bonus con attivazione delle due funzioni su html
 
 
+var start = document.getElementById('start');
 
 
+start.addEventListener('click',
+    function(){
+        var gioco = document.getElementById('gioco').value;
+        if (gioco == 'palindromo'){
+            alert(palindrom(prompt('inserisci parola')));
+            
+        }
+       else{
+           var scommessa =prompt('pari o dispari?');
+           var num_tuo = parseInt(prompt('dimmi un numero'));
+           var num_pc = rdn();
+           console.log(num_pc);
+           somma = pa_di(num_pc, num_tuo);
+           console.log(somma);
+           if (somma == scommessa){
+               console.log( 'Hai detto ' + scommessa + ' Tu hai fatto ' + num_tuo +' Il pc ha fatto ' + num_pc +' La somma è '+ somma + ' Hai vinto' )
+           }
+           else{
+            console.log( 'Hai detto ' + scommessa + ' Tu hai fatto ' + num_tuo +' Il pc ha fatto ' + num_pc + ' La somma è '+ somma + ' Hai perso' )
+           }
 
-
-
-// var start = document.getElementById('start');
-
-
-// start.addEventListener('click',
-//     function(){
-//         var gioco = document.getElementById('gioco').value;
-//         if (gioco == 'palindromo'){
-//             alert('ciao 1');
-//         }
-//        else{
-//            alert('noooo');
-//        }
-//     }
-// )
+       }
+    }
+)
